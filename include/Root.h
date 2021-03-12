2332fbd15595ed7f297747dd42d7a2b895b79446
lectroMathew <contact@lectro.moe>
2021-03-12 23:14:59 +0300

    Added core::Root class

---
+++ include/Root.h
@@ -0,0 +1,11 @@
+#ifndef INTERFACERS_LIB_CORE_INCLUDE_ROOT_H_
+#define INTERFACERS_LIB_CORE_INCLUDE_ROOT_H_
+
+#include "Singleton.h"
+
+class Root : public Singleton<Root> {
+
+};
+
+
+#endif //INTERFACERS_LIB_CORE_INCLUDE_ROOT_H_
