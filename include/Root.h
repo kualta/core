bd6b6f5f7658117ef7fe35ceb25b8d50ea8e767a
lectroMathew <contact@lectro.moe>
2021-03-15 23:24:18 +0300

    Added pointers to Manager objects in core::Root

--- include/Root.h
+++ include/Root.h
@@ -13,6 +13,11 @@ protected:
     std::vector<std::shared_ptr<Window>> windowsPool_;
 
     std::unique_ptr<LogManager> logManager_;
+    std::unique_ptr<WindowManager> windowManager_;
+    std::unique_ptr<RenderManager> renderManager_;
+    std::unique_ptr<InputManager> inputManager_;
+    std::unique_ptr<SoundManager> soundManager_;
+    std::unique_ptr<NetworkManager> networkManager_;
 
 private:
 
