ed536c2f88e40f2ec74bceb9599cafea64c6887c
lectroMathew <contact@lectro.moe>
2021-04-04 19:53:19 +0300

    Added Matrix3 and Matrix4 constructors and identity constants

--- include/core/Matrix.h
+++ include/core/Matrix.h
@@ -17,18 +17,41 @@ protected:
 
 template<> class Matrix<3, 3, float> {
 public:
+    Matrix(
+        float m00, float m01, float m02,
+        float m10, float m11, float m12,
+        float m20, float m21, float m22 )
+    {
+        mtx[0][0] = m00; mtx[0][1] = m01; mtx[0][2] = m02;
+        mtx[1][0] = m10; mtx[1][1] = m11; mtx[1][2] = m12;
+        mtx[2][0] = m20; mtx[2][1] = m21; mtx[2][2] = m22;
+    }
 
+    static const Matrix3 zero;
+    static const Matrix3 identity;
 protected:
     float mtx[3][3];
 };
 
 template<> class Matrix<4, 4, float> {
 public:
+    Matrix(
+        float m00, float m01, float m02, float m03,
+        float m10, float m11, float m12, float m13,
+        float m20, float m21, float m22, float m23,
+        float m30, float m31, float m32, float m33 )
+    {
+        mtx[0][0] = m00; mtx[0][1] = m01; mtx[0][2] = m02; mtx[0][3] = m03;
+        mtx[1][0] = m10; mtx[1][1] = m11; mtx[1][2] = m12; mtx[1][3] = m13;
+        mtx[2][0] = m20; mtx[2][1] = m21; mtx[2][2] = m22; mtx[2][3] = m23;
+        mtx[3][0] = m30; mtx[3][1] = m31; mtx[3][2] = m32; mtx[3][3] = m33;
+    }
 
+    static const Matrix4 zero;
+    static const Matrix4 identity;
 protected:
-    float mtx[3][3];
+    float mtx[4][4];
 };
-
 
 
 } // namespace core
--- src/Matrix.cpp
+++ src/Matrix.cpp
@@ -8,9 +8,30 @@ template<int cols, int rows, typename T>
 Matrix3 Matrix<cols, rows, T>::Translate(const Vector3 &v) {
 
 }
+const Matrix3 Matrix3::zero(
+    0, 0, 0,
+    0, 0, 0,
+    0, 0, 0,
+    0, 0, 0,);
+static const Matrix3 Matrix3::identity(
+    1, 0, 0,
+    0, 1, 0,
+    0, 0, 1,
+    0, 0, 0,);
 template<int cols, int rows, typename T>
 Matrix4 Matrix<cols, rows, T>::Translate(const Vector4 &v) {
 
 }
+
+const Matrix4 Matrix4::zero(
+    0, 0, 0, 0,
+    0, 0, 0, 0,
+    0, 0, 0, 0,
+    0, 0, 0, 0 );
+static const Matrix4 Matrix4::identity(
+    1, 0, 0, 0,
+    0, 1, 0, 0,
+    0, 0, 1, 0,
+    0, 0, 0, 1 );
 
 } // namespace
