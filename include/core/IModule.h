92f9a02a2596d59c1b440a7ea2b200e6d0522745
lectroMathew <contact@lectro.moe>
2021-04-24 17:01:01 +0300

    Added M_STATUS enum

--- include/core/IModule.h
+++ include/core/IModule.h
@@ -1,17 +1,31 @@
 #ifndef CORE_IMODULE_H
 #define CORE_IMODULE_H
 
+#include "Essential.h"
+
 namespace core {
 
-enum MOD_STATE {
-
+enum M_STATUS {
+    C_SUCCESS = 0,
+    C_STARTUP_FAILED,
+    C_UNRESOLVED_DEPENDENCY,
+    C_INVALID_INPUT,
+};
+struct StatusDesc {
+    M_STATUS code;
+    string message;
+} statusDesc[] = {
+        { C_SUCCESS, "No error" },
+        { C_STARTUP_FAILED, "Could not start module" },
+        { C_UNRESOLVED_DEPENDENCY, "Module depedencies were not resolved properly" },
+        { C_INVALID_INPUT, "Invalid input" },
 };
 
 class IModule {
 public:
 
-    virtual MOD_STATE Start() = 0;
-    virtual MOD_STATE Stop() = 0;
+    virtual M_STATUS Start() = 0;
+    virtual M_STATUS Stop() = 0;
 
 };
 
