8ac6924e55afadd6be17566d9b8e2d505e538945
lectroMathew <contact@lectro.moe>
2021-05-17 01:13:12 +0300

    core::Camera implementations, closes #15

--- include/core/Components/Camera.h
+++ include/core/Components/Camera.h
@@ -11,15 +11,64 @@ namespace core {
 
 class Camera : public Component {
 public:
-    Camera(Entity& parent);
+    Camera(Entity& parent,
+           float fovY = 90.0f,
+           float width = 600.0f,
+           float height = 400.0f,
+           float near = 0.1f,
+           float far = 100.0f,
+           bool hmgDepth = bgfx::getCaps()->homogeneousDepth);
+
+    void Start();
 
     void Update() override;
 
-    Matrix4 view;
-    Matrix4 proj;
+    void LookAt(Vector3& point);
+    void LookAt(Vector3&& point);
 
 protected:
+    Matrix4 viewMatrix;
+    Matrix4 projectionMatrix;
 
+    /**
+     * Point camera is pointing towards
+     */
+    Vector3 lookAt = Vector3::zero;
+
+    /**
+     * Field of View by Y axis
+     */
+    float fov = 90.0f;
+
+    /**
+     * Width of camera rectangle
+     */
+    float width = 600.0f;
+
+    /**
+     * Height of camera rectangle
+     */
+    float height = 400.0f;
+
+    /**
+     * Camera boundaries aspect ratio
+     */
+    float aspectRatio = width / height;
+
+    /**
+     * Near clipping plane distance
+     */
+    float nearPlane = 0.1f;
+
+    /**
+     * Far clipping plane distance
+     */
+    float farPlane = 100.0f;
+
+    /**
+     * Is homogeneous depth supported?
+     */
+    bool homogeneousDepth = bgfx::getCaps()->homogeneousDepth;
 };
 
 }
--- src/Components/Camera.cpp
+++ src/Components/Camera.cpp
@@ -1,18 +1,32 @@
 #include <core/Components/Camera.h>
+#include <core/Color.h>
 
 namespace core {
 
-Camera::Camera(Entity &parent) : Component(parent) {
+Camera::Camera(Entity &parent, float fovY, float width, float height, float near, float far, bool hmgDepth)
+: Component(parent), fov(fovY), aspectRatio(width/height), width(width), height(height),
+  nearPlane(near), farPlane(far), homogeneousDepth(hmgDepth) {
 
+    this->Start();
 }
-void core::Camera::Update() {
-    view = Math::LookAtMatrix(entity->transform->position,
-                              entity->transform->rotation,
-                              Vector3(0, 1, 0));
+void Camera::Update() {
+    viewMatrix = Math::LookAtMatrix(entity->transform->position, lookAt,Vector3(0, 1, 0));
 
-    proj = Math::ProjectionMatrix(60.0f, float(600)/ float(400), 0.1f, 100.0f, bgfx::getCaps()->homogeneousDepth);
+    projectionMatrix = Math::ProjectionMatrix(60.0f, float(600) / float(400), 0.1f, 100.0f, homogeneousDepth);
 
-    bgfx::setViewTransform(0, view.data, proj.data);
+    bgfx::setViewTransform(0, viewMatrix.data, projectionMatrix.data);
+}
+void Camera::LookAt(Vector3& point) {
+    lookAt = point;
+}
+void Camera::LookAt(Vector3 &&point) {
+    lookAt = point;
+}
+void Camera::Start() {
+    bgfx::reset(width, height, BGFX_RESET_VSYNC);
+    bgfx::setViewClear(0, BGFX_CLEAR_COLOR | BGFX_CLEAR_DEPTH, RgbaColor::corePurple, 1.0f, 0);
+    bgfx::setViewRect(0, 0, 0, width, height);
+    bgfx::frame();
 }
 
 
