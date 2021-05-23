4180adbefa19b0588c9fb8a493fb86f4cfe29946
lectroMathew <contact@lectro.moe>
2021-05-23 11:01:05 +0300

    Added core::ScriptBehaviour, Implemented Script.h

--- include/core/Components/Script.h
+++ include/core/Components/Script.h
@@ -3,14 +3,39 @@
 
 #include <core/Essential.h>
 #include <core/Component.h>
+#include <core/ScriptBehaviour.h>
 
 namespace core {
 
+template<typename T>
 class Script : public Component {
+    static_assert(std::is_base_of<core::ScriptBehaviour, T>::value, "Script T must inherit from ScriptBehaviour");
+
 public:
+    Script(Entity& parent, const string& name = "Script");
+    ~Script();
 
+    void FixedTick() { scriptObj->FixedTick(); }
 
-}; // class
+    void Tick() { scriptObj->Tick(); };
+
+    void LateTick() { scriptObj->LateTick(); }
+
+protected:
+
+    ScriptBehaviour* scriptObj;
+};
+
+template<typename T>
+Script<T>::Script(Entity &parent, const string &name) : Component(parent, name + " component") {
+    scriptObj = new T;
+    scriptObj->SetEntity(entity);
+}
+template<typename T>
+Script<T>::~Script() {
+    delete scriptObj;
+}
+// class
 
 } // namespace core
 
