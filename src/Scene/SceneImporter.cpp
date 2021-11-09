8de796a1e81ea618ef67269b58e63dedf065e55e
lectroMathew <contact@lectro.moe>
2021-11-10 02:46:32 +0300

    SceneImporter refactoring

--- src/Scene/SceneImporter.cpp
+++ src/Scene/SceneImporter.cpp
@@ -46,7 +46,10 @@ SceneImporter::SceneImporter() {
     InitImporter();
 }
 void SceneImporter::InitImporter() {
+    if (importer) { return; }
+    
     importer = manager.loadAndInstantiate("AnySceneImporter");
+    
     if (!importer) {
         Logger::Log(IMPORT, ERR_HERE) << "Failed to load or instantiate importer";
         throw std::runtime_error("Failed to load or instantiate importer");
@@ -60,26 +63,27 @@ void SceneImporter::OpenFile(const string& filepath) {
     }
     Logger::Log(IMPORT, DEBUG) << "Opened file " << filepath;
 }
-shared<SceneData> SceneImporter::ImportScene(const string& filepath) {
-    if (!importer) { InitImporter(); }
+void SceneImporter::CloseFile() {
+    importer->close();
+}
+shared<SceneData> SceneImporter::Import(const string& filepath) {
+    shared<SceneData> data = make_shared<SceneData>();
     OpenFile(filepath);
     
-    shared<SceneData> data = make_shared<SceneData>();
     ImportChildrenData(*data);
     ImportObjectData(*data);
     ImportMaterials(*data);
     ImportTextures(*data);
     ImportMeshes(*data);
     
-    importer->close();
-    
+    CloseFile();
     return data;
 }
 void SceneImporter::ImportTextures(SceneData& data) {
-    data.textures = std::vector<std::optional<GL::Texture2D>> { importer->textureCount() };
+    data.textures = vector<optional<GL::Texture2D>> { importer->textureCount() };
     
-    for (uint32_t i = 0; i != importer->textureCount(); ++i) {
-        std::optional<Trade::TextureData> textureData = std::optional<Trade::TextureData>(importer->texture(i));
+    for (uint32_t i = 0; i != importer->textureCount(); i++) {
+        optional<Trade::TextureData> textureData = optional<Trade::TextureData>(importer->texture(i));
         if (!textureData) {
             Logger::Log(IMPORT, WARN) << "[" << i << "] " << "failed to load, skipping";
             continue;
@@ -94,8 +98,8 @@ void SceneImporter::ImportTextures(SceneData& data) {
                                                        << (int)textureData->wrapping().y();
     
         const uint32_t imageID = textureData->image();
-        const std::optional<Trade::ImageData2D> imageData = std::optional<Trade::ImageData2D>(importer->image2D(imageID));
-        const GL::TextureFormat format = GetTextureFormat(imageData->format());
+        const optional<Trade::ImageData2D> imageData = optional<Trade::ImageData2D>(importer->image2D(imageID));
+        const GL::TextureFormat format { imageData->format() };
         if (!imageData) {
             Logger::Log(IMPORT, WARN_HERE) << " | Image " << imageID << " failed to load, skipping";
             continue;
@@ -115,10 +119,10 @@ void SceneImporter::ImportTextures(SceneData& data) {
     }
 }
 void SceneImporter::ImportMaterials(SceneData& data) {
-    data.materials = std::vector<std::optional<Trade::MaterialData>> { importer->materialCount() };
+    data.materials = vector<optional<Trade::MaterialData>> { importer->materialCount() };
     for (uint32_t i = 0; i != importer->materialCount(); ++i) {
    
-        std::optional<Trade::MaterialData> materialData = std::optional<Trade::MaterialData>(importer->material(i));
+        optional<Trade::MaterialData> materialData = optional<Trade::MaterialData>(importer->material(i));
         if (!materialData) {
             Logger::Log(IMPORT, WARN) << "[" << i << "] Material failed to load, skipping";
             continue;
@@ -150,9 +154,9 @@ void SceneImporter::ImportMaterials(SceneData& data) {
     }
 }
 void SceneImporter::ImportMeshes(SceneData& data) {
-    data.meshes = std::vector<std::optional<GL::Mesh>> { importer->meshCount() };
+    data.meshes = vector<optional<GL::Mesh>> { importer->meshCount() };
     for (uint32_t i = 0; i != importer->meshCount(); ++i) {
-        std::optional<Trade::MeshData> meshData = std::optional<Trade::MeshData>(importer->mesh(i));
+        optional<Trade::MeshData> meshData = optional<Trade::MeshData>(importer->mesh(i));
         if (!meshData) {
             Logger::Log(IMPORT, WARN_HERE) << "[" << i << "] Mesh failed to load, skipping";
             continue;
@@ -167,8 +171,8 @@ void SceneImporter::ImportMeshes(SceneData& data) {
     }
 }
 void SceneImporter::ImportObjectData(SceneData& data) {
-    data.objects = std::vector<unique<Trade::ObjectData3D>> { importer->object3DCount() };
-    data.names = std::vector<string> { importer->object3DCount() };
+    data.objects = vector<unique<Trade::ObjectData3D>> { importer->object3DCount() };
+    data.names = vector<string> { importer->object3DCount() };
     for (uint32_t i = 0; i != importer->object3DCount(); ++i) {
         unique<Trade::ObjectData3D> objectData = importer->object3D(i);
         string name = importer->object3DName(i);
@@ -185,77 +189,12 @@ void SceneImporter::ImportObjectData(SceneData& data) {
 void SceneImporter::ImportChildrenData(SceneData& data) {
     if (importer->defaultScene() != -1) {
         Logger::Log(IMPORT, INFO) << "Importing Default Scene " << importer->sceneName(importer->defaultScene());
-        data.children = std::optional<Trade::SceneData>(importer->scene(importer->defaultScene()));
+        data.children = optional<Trade::SceneData>(importer->scene(importer->defaultScene()));
         if (!data.children) {
             Logger::Log(IMPORT, ERR_HERE) << "Children data failed to load";
             throw std::runtime_error("Cannot load scene, Children data failed to load");
         }
     }
-}
-GL::TextureFormat SceneImporter::GetTextureFormat(PixelFormat pixelFormat) {
-    GL::TextureFormat format;
-    switch (pixelFormat) {
-        case PixelFormat::R8Unorm:      format = GL::TextureFormat::R8; break;
-        case PixelFormat::RG8Unorm:     format = GL::TextureFormat::RG8; break;
-        case PixelFormat::RGB8Unorm:    format = GL::TextureFormat::RGB8; break;
-        case PixelFormat::RGBA8Unorm:   format = GL::TextureFormat::RGBA8; break;
-        case PixelFormat::R8Snorm:      format = GL::TextureFormat::R8Snorm; break;
-        case PixelFormat::RG8Snorm:     format = GL::TextureFormat::RG8Snorm; break;
-        case PixelFormat::RGB8Snorm:    format = GL::TextureFormat::RGB8Snorm; break;
-        case PixelFormat::RGBA8Snorm:   format = GL::TextureFormat::RGBA8Snorm; break;
-        case PixelFormat::R8Srgb:       format = GL::TextureFormat::SR8; break;
-        case PixelFormat::RGB8Srgb:     format = GL::TextureFormat::SRGB8; break;
-        case PixelFormat::RGBA8Srgb:    format = GL::TextureFormat::SRGB8Alpha8; break;
-        case PixelFormat::R8UI:         format = GL::TextureFormat::R8UI; break;
-        case PixelFormat::RG8UI:        format = GL::TextureFormat::RG8UI; break;
-        case PixelFormat::RGB8UI:       format = GL::TextureFormat::RGB8UI; break;
-        case PixelFormat::RGBA8UI:      format = GL::TextureFormat::RGBA8UI; break;
-        case PixelFormat::R8I:          format = GL::TextureFormat::R8I; break;
-        case PixelFormat::RG8I:         format = GL::TextureFormat::RG8I; break;
-        case PixelFormat::RGB8I:        format = GL::TextureFormat::RGB8I; break;
-        case PixelFormat::RGBA8I:       format = GL::TextureFormat::RGBA8I; break;
-        case PixelFormat::R16Unorm:     format = GL::TextureFormat::R16; break;
-        case PixelFormat::RG16Unorm:    format = GL::TextureFormat::RG16; break;
-        case PixelFormat::RGB16Unorm:   format = GL::TextureFormat::RGB16; break;
-        case PixelFormat::RGBA16Unorm:  format = GL::TextureFormat::RGBA16; break;
-        case PixelFormat::R16Snorm:     format = GL::TextureFormat::R16Snorm; break;
-        case PixelFormat::RG16Snorm:    format = GL::TextureFormat::RG16Snorm; break;
-        case PixelFormat::RGB16Snorm:   format = GL::TextureFormat::RGB16Snorm; break;
-        case PixelFormat::RGBA16Snorm:  format = GL::TextureFormat::RGBA16Snorm; break;
-        case PixelFormat::R16UI:        format = GL::TextureFormat::R16UI; break;
-        case PixelFormat::RG16UI:       format = GL::TextureFormat::RG16UI; break;
-        case PixelFormat::RGB16UI:      format = GL::TextureFormat::RGB16UI; break;
-        case PixelFormat::RGBA16UI:     format = GL::TextureFormat::RGBA16UI; break;
-        case PixelFormat::R16I:         format = GL::TextureFormat::R16I; break;
-        case PixelFormat::RG16I:        format = GL::TextureFormat::RG16I; break;
-        case PixelFormat::RGB16I:       format = GL::TextureFormat::RGB16I; break;
-        case PixelFormat::RGBA16I:      format = GL::TextureFormat::RGBA16I; break;
-        case PixelFormat::R32UI:        format = GL::TextureFormat::R32UI; break;
-        case PixelFormat::RG32UI:       format = GL::TextureFormat::RG32UI; break;
-        case PixelFormat::RGB32UI:      format = GL::TextureFormat::RGB32UI; break;
-        case PixelFormat::RGBA32UI:     format = GL::TextureFormat::RGBA32UI; break;
-        case PixelFormat::R32I:         format = GL::TextureFormat::R32I; break;
-        case PixelFormat::RG32I:        format = GL::TextureFormat::RG32I; break;
-        case PixelFormat::RGB32I:       format = GL::TextureFormat::RGB32I; break;
-        case PixelFormat::RGBA32I:      format = GL::TextureFormat::RGBA32I; break;
-        case PixelFormat::R16F:         format = GL::TextureFormat::R16F; break;
-        case PixelFormat::RG16F:        format = GL::TextureFormat::RG16F; break;
-        case PixelFormat::RGB16F:       format = GL::TextureFormat::RGB16F; break;
-        case PixelFormat::RGBA16F:      format = GL::TextureFormat::RGBA16F; break;
-        case PixelFormat::R32F:         format = GL::TextureFormat::R32F; break;
-        case PixelFormat::RG32F:        format = GL::TextureFormat::RG32F; break;
-        case PixelFormat::RGB32F:       format = GL::TextureFormat::RGB32F; break;
-        case PixelFormat::RGBA32F:      format = GL::TextureFormat::RGBA32F; break;
-        case PixelFormat::Depth32F:     format = GL::TextureFormat::DepthComponent32F; break;
-        case PixelFormat::Stencil8UI:   format = GL::TextureFormat::StencilIndex8; break;
-        case PixelFormat::Depth16Unorm: format = GL::TextureFormat::DepthComponent16; break;
-        case PixelFormat::Depth24Unorm: format = GL::TextureFormat::DepthComponent24; break;
-        case PixelFormat::Depth24UnormStencil8UI: format = GL::TextureFormat::Depth24Stencil8; break;
-        case PixelFormat::Depth32FStencil8UI: format = GL::TextureFormat::Depth32FStencil8; break;
-        case PixelFormat::Depth16UnormStencil8UI: throw std::logic_error("Unsupported pixel format"); // VK-speciffic
-        case PixelFormat::RG8Srgb: throw std::logic_error("Unsupported pixel format"); // < GL::TextureFormat::SRG8 is not a thing yet, FIXME: Update magnum to upstream
-    }
-    return format;
 }
 
 }
